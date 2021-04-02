21e1be018d160893a71487f029e8c2207e14b04b
swha <hsw636@gmail.com>
2021-04-02 09:37:47 +0900

    선수 데이터 추출 코드 완료

--- get_playerdata_from_html.py
+++ get_playerdata_from_html.py
@@ -5,9 +5,9 @@ import pandas as pd
 import os
 import sys
 
-
-
 #%%
+
+
 ## 타자 
 def batter_get_from_soup(soup):
 
@@ -27,10 +27,9 @@ def batter_get_from_soup(soup):
             for rows in table.find_all('span'):
                 tmp.append(rows.get_text())
             batterInfo = batterInfo.append([tmp])
-
+    batterInfo.columns = featureName
     return batterInfo
 
-#%%
 
 
 
@@ -41,7 +40,7 @@ years = ['2016','2017','2018','2019','2020']
 playerList = os.listdir(data_path)
 NameBirth = list(map(lambda x:(x.split('_')[0],  x.split('_')[1] ),playerList ))
 
-#%%
+
 for name,birth in NameBirth:
 
     Info = pd.DataFrame()
@@ -68,37 +67,69 @@ for name,birth in NameBirth:
                     Info.to_excel(tmp,sheet_name=year,index=False)
 
 
-    
+
+## 투수 
+
+def pitcher_get_from_soup(soup):
+
+    featureName = ['날짜','상대','결과','선발','이닝','실점','자책','타자','타수','안타'\
+        ,'2타','3타','홈런','볼넷','고4','사구','삼진','투구','WHIP','타율','출루율'\
+        ,'OPS','ERA','avLI','RE24','WPA','GSC','DEC','간격']
+    feature = []
+
+    pitcher_table = soup.select_one('body > div.wrapper > div.content-wrapper > div > section.content > div > div:nth-child(2) > div > div:nth-child(3) > div > div > table')
+    pitcherInfo = pd.DataFrame()
 
 
-#%%
-
-
-tmp_file = '/home/swha/ToTo/ToTo/Data/soup/picther_soup_data/차우찬_1987-05-31_2017'
-soup = BeautifulSoup(open(tmp_file),"html.parser")
-pitcher_table = soup.select_one('body > div.wrapper > div.content-wrapper > div > section.content > div > div:nth-child(2) > div > div:nth-child(3) > div > div > table')
-
-
-pitcher_table = soup.select_one('body > div.wrapper > div.content-wrapper > div > section.content > div > div:nth-child(2) > div > div:nth-child(3) > div > div > table')
-
-
-
-featureName = ['상대','결과','선발','이닝','실점','자책','타자','타수','안타'\
-    ,'2타','3타','홈런','볼넷','고4','사구','삼진','투구','WHIP','타율','출루율'\
-    ,'OPS','ERA','avLI','RE24','WPA','GSC','DEC','간격']
-feature = []
-
-for n,table in enumerate(pitcher_table.find_all('tr')):
-    
-    if table.find_all('span')[-1].get_text() == '간격':
-        print('it will pass')
-    else:
-        tmp = []
-        for rows in table.find_all('span'):
-            tmp.append(rows.get_text())
-        feature.append(tmp)
+    for table in pitcher_table.find_all('tr'):
         
-        # if n == 2:
-        #     break
+        if table.find_all('span')[-1].get_text() == '간격':
+            continue
+            
+        else:
+            tmp = []
+            
+            for rows in table.find_all('span'):
+                tmp.append(rows.get_text())
+            pitcherInfo = pitcherInfo.append([tmp])
+    pitcherInfo.columns = featureName
+
+    return pitcherInfo    
 
 
+
+data_path = '/home/swha/ToTo/ToTo/Data/soup/picther_soup_data/'
+save_path = '/home/swha/ToTo/ToTo/Data/raw/picther/'
+years = ['2016','2017','2018','2019','2020']
+
+playerList = os.listdir(data_path)
+NameBirth = list(map(lambda x:(x.split('_')[0],  x.split('_')[1] ),playerList ))
+
+
+for name,birth in NameBirth:
+
+    Info = pd.DataFrame()
+
+    flag = 0    
+    for year in years:
+        print(name,year)
+        fileName = f'{name}_{birth}_{year}'
+        try:
+            soup = BeautifulSoup(open(data_path + fileName),"html.parser")
+            Info = (pitcher_get_from_soup(soup))
+        except FileNotFoundError:
+            pass
+
+        if len(Info) == 0:
+            continue   
+        else:
+            if flag == 0:
+                with pd.ExcelWriter(f'{save_path}{name}_{birth}.xlsx', engine='openpyxl') as tmp:
+                    Info.to_excel(tmp,sheet_name=year,index=False)
+                    flag = 1 
+            else:
+                with pd.ExcelWriter(f'{save_path}{name}_{birth}.xlsx',mode='a', engine='openpyxl') as tmp:
+                    Info.to_excel(tmp,sheet_name=year,index=False)
+
+
+    
